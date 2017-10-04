16767206ba1a40dbe217ec9e16b052c848f84b10
MelomanCool <melomancool@gmail.com>
2017-10-04 23:34:35 +0400

    Use libopus codec while converting to Voice

--- converter.py
+++ converter.py
@@ -5,7 +5,7 @@ from io import BytesIO
 def convert_to_ogg(f):
     bio = BytesIO()
 
-    AudioSegment.from_file(f).export(bio, format='ogg')
+    AudioSegment.from_file(f).export(bio, format='ogg', codec='libopus')
     bio.seek(0)
 
     return bio
