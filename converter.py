ff203f6624300ba43a90dea10f76c8de18ff52f2
MelomanCool <melomancool@gmail.com>
2017-11-15 01:22:55 +0400

    Fix playback on Android

--- converter.py
+++ converter.py
@@ -5,7 +5,9 @@ from io import BytesIO
 def convert_to_ogg(f):
     bio = BytesIO()
 
-    AudioSegment.from_file(f).export(bio, format='ogg', codec='libopus')
+    (AudioSegment.from_file(f)
+                 .set_channels(1)  # for compatibility with Android
+                 .export(bio, format='ogg', codec='libopus'))
     bio.seek(0)
 
     return bio
